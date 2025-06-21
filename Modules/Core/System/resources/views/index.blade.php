83745f88995e8ebb3bcf0161af1aca1725591044
ahmedelasley <ahmedelasley@gmail.com>
2025-06-21 14:49:49 +0300

    Delete Core/System Module

--- Modules/Core/System/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-system::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('system.name') !!}</p>
-</x-system::layouts.master>
--- Modules/Core/System/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\\Core\\System\Http\Controllers\SystemController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('systems', SystemController::class)->names('system');
-});
