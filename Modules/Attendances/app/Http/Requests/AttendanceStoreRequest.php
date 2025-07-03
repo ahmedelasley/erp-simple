4b8dcadc514e8a3d83883849d898e4a42f7d67b8
ahmedelasley <ahmedelasley@gmail.com>
2025-07-03 14:08:39 +0300

    create Requests in Attendances Module

---
+++ Modules/Attendances/app/Http/Requests/AttendanceStoreRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Attendances\Http\Requests;
+use Illuminate\Validation\Rules\Enum;
+use Modules\Attendances\Enums\AttendanceStatus;
+use Modules\Core\Http\Requests\BaseStoreRequest;
+
+class AttendanceStoreRequest extends BaseStoreRequest
+{
+
+    /**
+     * Common rules for storing data.
+     *
+     * @return array
+     */
+    public function rules() :array
+    {
+        return [
+            'employee_id' => 'required|integer|min:1|exists:employees,id',
+            'check_in'      => 'required|date|before_or_equal:check_out',
+            'check_out'     => 'required|date|after_or_equal:check_in',
+            'hours_worked'  => 'required|numeric|min:0|max:999999.99',
+            'date'          => 'required|date|before_or_equal:today',
+            'status' => [new Enum(AttendanceStatus::class)],
+        ];
+
+    }
+}
