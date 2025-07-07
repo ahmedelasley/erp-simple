36f5832ac9b5cee0e6ba5c8dea6b33d4ea728f0a
ahmedelasley <ahmedelasley@gmail.com>
2025-07-08 01:03:24 +0300

    Refactor Select Component in Core Module

--- Modules/Core/resources/views/components/form/fields/select.blade.php
+++ Modules/Core/resources/views/components/form/fields/select.blade.php
@@ -2,27 +2,51 @@
     'name',
     'label' => __('Select...'),
     'placeholder' => __('Select...'),
-    'class' => '',
-    'livewire' => false,
-
+    'divClass' => '',  // فئة div الافتراضية لتحسين الهيكلية
+    'labelClass' => '', // فئة التسمية الافتراضية لتنسيق متناسق
+    'selectClass' => '', // فئة الـ select الافتراضية للاتساق
+    'otherAttributes' => '',  // إعادة التسمية إلى جمع لتكون متوافقة مع الاتفاقيات
+    'isLabel' => true,
+    'isLivewire' => false,
+    'isError' => true,
 ])
 
-<div class="mb-3">
-    <label class="main-content-label tx-12 tx-medium">{{ __($label) }}</label>
+<div {{ $attributes->merge(['class' => 'form-group mb-3 ' . $divClass]) }}>
+    @if($isLabel)
+        <label for="{{ $name }}" {{ $attributes->merge(['class' => 'main-content-label tx-12 tx-medium ' . $labelClass]) }}>
+            {{ __($label) }}
+        </label>
+    @endif
 
     <select name="{{ $name }}" id="{{ $name }}"
-        @if($livewire)
-            wire:model.live='{{ $name }}'
+        @if($isLivewire)
+            wire:model.live="{{ $name }}"
         @endif
-        {{ $attributes->merge(['class' => 'form-control ' . $class]) }}>
-        <option value="">{{ __($placeholder) }}</option>
+        {{ $attributes->merge(['class' => 'form-control ' . $selectClass]) }} {{ $otherAttributes }}>
+        <option value="" disabled selected hidden>{{ __($placeholder) }}</option>
         {{ $slot }}
     </select>
 
-    @error($name)
-        <small class="text-danger d-block mt-1">{{ $message }}</small>
-    @enderror
+    @if($isError)
+        @error($name)
+            <small class="text-danger d-block mt-1">{{ $message }}</small>
+        @enderror
+    @endif
 </div>
 
-{{-- Usage example --}}
 
+{{-- Usage Example --}}
+{{--
+<x-core::form.fields.select
+    name="country"
+    label="Label"
+    placeholder="Select Label"
+    :isLivewire="true"
+    divClass="custom-class"
+    labelClass="label-custom"
+    selectClass="select-custom"
+    otherAttributes="data-example='value'"
+>
+Slot
+</x-core::form.fields.select>
+--}}
