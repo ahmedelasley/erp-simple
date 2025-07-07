afcd623fa5490d9f7467570db6b6325b2c7d1523
ahmedelasley <ahmedelasley@gmail.com>
2025-07-08 01:05:24 +0300

    Edit select at form (create - edit) in Positions Module

--- Modules/Positions/resources/views/livewire/positions/partials/edit.blade.php
+++ Modules/Positions/resources/views/livewire/positions/partials/edit.blade.php
@@ -1,23 +1,39 @@
 <!-- Start Edit modal -->
-<form id="form">
+<form id="form" class="row">
     <x-core::form.fields.input-label-error type="text"  livewire="true" name="name" placeholder="Enter Name"  label="Name" value="{{old('name')}}" required autofocus autocomplete="name"/>
-    <x-core::form.fields.input-label-error type="text"  livewire="true" name="description" placeholder="Enter Description"  label="Description" value="{{old('description')}}" required />
-    <x-core::form.fields.select name="level" livewire="true" label='Position Level' placeholder="Select a Level..." class="" >
+    <x-core::form.fields.input-label-error type="text"  livewire="true" name="description" placeholder="Enter Description" label="Description" value="{{old('description')}}" required />
+    <x-core::form.fields.select
+        name="level"
+        label="Position Level"
+        placeholder="Select a Level..."
+        :isLivewire="true"
+        divClass="col-md-4"
+        labelClass=""
+        selectClass=""
+        otherAttributes=""
+    >
         @forelse (\Modules\Positions\Enums\PositionLevel::options() as $value => $label)
             <option value="{{ $value }}" wire:key="level-{{ $value }}" >{{ $label }}</option>
         @empty
             <option value="0" wire:key="level-none">None</option>
         @endforelse
     </x-core::form.fields.select>
-
-    <x-core::form.fields.select name="department_id" livewire="true" label='Department' placeholder="Select a Department..." class="" >
+    <x-core::form.fields.select
+        name="department_id"
+        label="Department"
+        placeholder="Select a Department..."
+        :isLivewire="true"
+        divClass="col-md-8"
+        labelClass=""
+        selectClass=""
+        otherAttributes=""
+    >
         @forelse ($data as $value)
             <option value="{{ $value->id }}" wire:key="department-{{ $value->id }}" >{{ $value->name }}</option>
         @empty
             <option value="0" wire:key="department-none">None</option>
         @endforelse
     </x-core::form.fields.select>
-
 
 </form>
 <!-- End Edit modal -->
