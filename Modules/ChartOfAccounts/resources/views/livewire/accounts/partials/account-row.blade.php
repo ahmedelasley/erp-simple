ca4ef23716a16a1ba38341d68c118519f9547f90
ahmedelasley <ahmedelasley@gmail.com>
2025-07-11 23:27:20 +0300

    Make account-row in GetData component in ChartOfAccounts Module

---
+++ Modules/ChartOfAccounts/resources/views/livewire/accounts/partials/account-row.blade.php
@@ -0,0 +1,17 @@
+
+@php
+    // $balance = $account->getRecursiveBalance();
+    $indent = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $level);
+@endphp
+<tr class="tx-bold tx-16">
+    <td class="text-start" style="padding-left: {{ $level * 35 }}px;font-weight:bolde;">
+        <span class="text-muted tx-bold tx-16 me-5">{{ $account->code }}</span>
+        <span class="text-wrap tx-bold tx-16"> - {{ $account->name }}</span>
+    </td>
+
+    <td class="text-center" >0.00</td>
+</tr>
+
+@foreach ($account->children as $child)
+    @include('chartofaccounts::livewire.accounts.partials.account-row', ['account' => $child, 'level' => $level + 1])
+@endforeach
