f87d37581ffa680aeea687e64c93267b65339dfb
ahmedelasley <ahmedelasley@gmail.com>
2025-07-12 14:13:23 +0300

    Change in AccountType enum in ChartOfAccounts Module

--- Modules/ChartOfAccounts/app/Enums/AccountType.php
+++ Modules/ChartOfAccounts/app/Enums/AccountType.php
@@ -10,6 +10,6 @@ enum AccountType : string
     case ASSET     = 'asset';
     case LIABILITY = 'liability';
     case EQUITY    = 'equity';
-    case INCOME    = 'income';
+    case REVENUE    = 'revenue';
     case EXPENSE   = 'expense';
 }
